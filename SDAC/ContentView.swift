003c389804c80dab42d5b9f26dac2e7bc4c7c8d2
Wen Peng Lee <leewenpengl@gmail.com>
2023-10-25 20:52:29 +0800

    Initial Commit

---
+++ SDAC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDAC
+//
+//  Created by Wen Peng Lee on 2023/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDAC/SDACApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDACApp.swift
+//  SDAC
+//
+//  Created by Wen Peng Lee on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SDACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
